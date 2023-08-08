ff1c9eb3af708175b6ce94db1442048459f7d016
sorrymint <ldmatheis.00@gmail.com>
2023-08-08 09:02:51 -0500

    first commit

---
+++ src/main/java/Data.java
@@ -0,0 +1,12 @@
+public class Data {
+//    Instance variables amount, unit, and downloadSpeed
+
+
+//    Constructor
+
+
+//    Class methods
+
+
+//    toString using Class methods
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Build a scanner
+
+        //Prompt and store double amount
+
+        //Prompt and store String unit
+
+        //Prompt and store download speed (in megabits
+
+        //Build a Data object with these three values
+
+        //Print out the data Object with your toString();
+    }
+}
